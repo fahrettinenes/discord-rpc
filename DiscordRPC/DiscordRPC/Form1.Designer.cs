4285da3f2e143c25e02a3837a28bc1094859fe71
Fahrettin Enes <76036578+fahrettinenes@users.noreply.github.com>
2021-02-21 19:06:32 +0300

    Add files via upload

---
+++ DiscordRPC/DiscordRPC/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace DiscordRPC1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.appID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.yazacakBaslik = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.yazacakAciklama = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resimURL = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.kucukResimURL = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buyukYazi = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.kucukYazi = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "App ID:";
+            // 
+            // appID
+            // 
+            this.appID.Location = new System.Drawing.Point(75, 24);
+            this.appID.Name = "appID";
+            this.appID.Size = new System.Drawing.Size(260, 20);
+            this.appID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(9, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Yazacak Açıklama:";
+            // 
+            // yazacakBaslik
+            // 
+            this.yazacakBaslik.Location = new System.Drawing.Point(149, 58);
+            this.yazacakBaslik.Name = "yazacakBaslik";
+            this.yazacakBaslik.Size = new System.Drawing.Size(260, 20);
+            this.yazacakBaslik.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(9, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Yazacak Başlık:";
+            // 
+            // yazacakAciklama
+            // 
+            this.yazacakAciklama.Location = new System.Drawing.Point(126, 94);
+            this.yazacakAciklama.Name = "yazacakAciklama";
+            this.yazacakAciklama.Size = new System.Drawing.Size(260, 20);
+            this.yazacakAciklama.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(9, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 21);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Resim Anahtar:";
+            // 
+            // resimURL
+            // 
+            this.resimURL.Location = new System.Drawing.Point(130, 129);
+            this.resimURL.Name = "resimURL";
+            this.resimURL.Size = new System.Drawing.Size(260, 20);
+            this.resimURL.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(9, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 21);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Küçük Resim Anahtar:";
+            // 
+            // kucukResimURL
+            // 
+            this.kucukResimURL.Location = new System.Drawing.Point(176, 164);
+            this.kucukResimURL.Name = "kucukResimURL";
+            this.kucukResimURL.Size = new System.Drawing.Size(260, 20);
+            this.kucukResimURL.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(9, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(309, 21);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Büyük resimin üzerine gelince yazacak yazı:";
+            // 
+            // buyukYazi
+            // 
+            this.buyukYazi.Location = new System.Drawing.Point(324, 199);
+            this.buyukYazi.Name = "buyukYazi";
+            this.buyukYazi.Size = new System.Drawing.Size(260, 20);
+            this.buyukYazi.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(9, 235);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(308, 21);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Küçük resimin üzerine gelince yazacak yazı:";
+            // 
+            // kucukYazi
+            // 
+            this.kucukYazi.Location = new System.Drawing.Point(324, 235);
+            this.kucukYazi.Name = "kucukYazi";
+            this.kucukYazi.Size = new System.Drawing.Size(260, 20);
+            this.kucukYazi.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(458, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Uygula";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(9, 270);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 21);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Buton 1 İsim:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 271);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(9, 296);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 21);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Buton 1 URL:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 297);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(260, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(328, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ayarla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.ClientSize = new System.Drawing.Size(594, 376);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.kucukYazi);
+            this.Controls.Add(this.buyukYazi);
+            this.Controls.Add(this.kucukResimURL);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.resimURL);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.yazacakAciklama);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.yazacakBaslik);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.appID);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(3)))), ((int)(((byte)(60)))), ((int)(((byte)(153)))));
+            this.Name = "Form1";
+            this.Text = "DiscordRPC by Fahrettin Enes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox appID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox yazacakBaslik;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox yazacakAciklama;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox resimURL;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox kucukResimURL;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox buyukYazi;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox kucukYazi;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DiscordRPC/DiscordRPC/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscordRPC;
+using DiscordRPC.Logging;
+
+namespace DiscordRPC1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public DiscordRPC.DiscordRpcClient client;
+        bool ayarlimi = false;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!ayarlimi)
+                MessageBox.Show("Lütfen ayarla butonuna basınız.");
+
+            else
+            {
+                DiscordRPC.Button[] buttons = new DiscordRPC.Button[]
+                {
+                new DiscordRPC.Button
+                {
+                    Label = textBox1.Text,
+                    Url = textBox2.Text
+                },
+                };
+
+                client.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    State = yazacakBaslik.Text,
+                    Details = yazacakAciklama.Text,
+                    Buttons = buttons,
+                    Assets = new Assets()
+                    {
+                        SmallImageKey = kucukResimURL.Text,
+                        SmallImageText = kucukYazi.Text,
+                        LargeImageKey = resimURL.Text,
+                        LargeImageText = buyukYazi.Text
+                    }
+                });
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ayarlimi = true;
+            client = new DiscordRPC.DiscordRpcClient(appID.Text);
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+        }
+    }
+}
